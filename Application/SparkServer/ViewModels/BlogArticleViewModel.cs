3eb061f9087aa53f2c4eba910c9e4707e7b8f0e4
bmbruno <bmbruno@gmail.com>
2017-01-15 07:12:35 -0500

    Set up BlogArticle view and viewmodel.

---
+++ Application/SparkServer/ViewModels/BlogArticleViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkServer.ViewModels
+{
+    public class BlogArticleViewModel
+    {
+        //
+        // Article Information
+        //
+
+        public int BlogID { get; set; }
+
+        public string ArticleUniqueURL { get; set; }
+
+        public string ArticleTitle { get; set; }
+
+        public string Body { get; set; }
+
+        public string SitecoreVersionShort { get; set; }
+
+        public string SitecoreVersionLong { get; set; }
+
+        public string SitecoreVersionDescription { get; set; }
+
+        public string AuthurFullName { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+        
+    }
+}
