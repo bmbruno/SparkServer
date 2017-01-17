d9017e16dca9ba7f2ee8fbad05cf7568dc10a0b8
bmbruno <bmbruno@gmail.com>
2017-01-16 20:39:45 -0500

    Created supporting enum and viewmodel for Blog list page.

---
+++ Application/SparkServer/Application/Enum/ViewMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkServer.Application.Enum
+{
+    public enum ViewMode
+    {
+        Overview = 0,
+        List = 1,
+        Compact = 2
+    }
+}
---
+++ Application/SparkServer/ViewModels/BlogListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using SparkServer.Application.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkServer.ViewModels
+{
+    public class BlogListViewModel
+    {
+        public List<BlogArticleViewModel> BlogList{ get; set; }
+
+        public string Year { get; set; }
+
+        public string Month { get; set; }
+
+        public ViewMode ViewMode { get; set; }
+
+        public BlogListViewModel()
+        {
+            BlogList = new List<BlogArticleViewModel>();
+        }
+    }
+}
