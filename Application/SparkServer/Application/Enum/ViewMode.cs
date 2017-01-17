ce3789ec709b87eb06fb6fd20f1c2b66131e2515
bmbruno <bmbruno@gmail.com>
2017-01-17 16:53:01 -0500

    Added comments to detail ViewMode values.

--- Application/SparkServer/Application/Enum/ViewMode.cs
+++ Application/SparkServer/Application/Enum/ViewMode.cs
@@ -7,8 +7,19 @@ namespace SparkServer.Application.Enum
 {
     public enum ViewMode
     {
+        /// <summary>
+        /// Used for full-view, dashboad-style, or multi-view data display.
+        /// </summary>
         Overview = 0,
+
+        /// <summary>
+        /// Used for list-only data display.
+        /// </summary>
         List = 1,
+
+        /// <summary>
+        /// Used for small or subset data display.
+        /// </summary>
         Compact = 2
     }
 }
