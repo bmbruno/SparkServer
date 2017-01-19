13c1547261deb0e444f2a546e43ac61de4c68ba2
bmbruno <bmbruno@gmail.com>
2017-01-19 13:58:04 -0500

    Added additional test blogs to SQL insert scripts.

--- SQLScripts/DDL-DML/INIT-DATA/40_Blog.sql
+++ SQLScripts/DDL-DML/INIT-DATA/40_Blog.sql
@@ -11,14 +11,71 @@ INSERT INTO [dbo].[Blog]
            ,[Active]
            ,[CreateDate])
      VALUES
-           ('Blog Article #1'
-           ,'Subtitle Test'
-           ,'<p>body!</p>'
-           ,'blog-article-one'
-           ,'2017-01-15'
-           ,1
-           ,1
-           ,'2017-01-14')
+           ('Blog Article #1' ,'Subtitle Test' ,'<p>body!</p>', 'blog-article-one' ,'2017-01-15' , 1 ,1 ,'2017-01-14')
+GO
+
+INSERT INTO [dbo].[Blog]
+           ([Title]
+           ,[Subtitle]
+           ,[Body]
+           ,[UniqueURL]
+           ,[PublishDate]
+           ,[AuthorID]
+           ,[Active]
+           ,[CreateDate])
+     VALUES
+           ('Blog Article #2' ,'Subtitle Test' ,'<p>body!</p>', 'blog-article-two' ,'2017-01-16' , 1 ,1 ,'2017-01-14')
 GO
 
 
+INSERT INTO [dbo].[Blog]
+           ([Title]
+           ,[Subtitle]
+           ,[Body]
+           ,[UniqueURL]
+           ,[PublishDate]
+           ,[AuthorID]
+           ,[Active]
+           ,[CreateDate])
+     VALUES
+           ('Blog Article #3' ,'Subtitle Test' ,'<p>body!</p>', 'blog-article-three' ,'2017-02-03' , 1 ,1 ,'2017-01-14')
+GO
+
+INSERT INTO [dbo].[Blog]
+           ([Title]
+           ,[Subtitle]
+           ,[Body]
+           ,[UniqueURL]
+           ,[PublishDate]
+           ,[AuthorID]
+           ,[Active]
+           ,[CreateDate])
+     VALUES
+           ('Blog Article #4' ,'Subtitle Test' ,'<p>body!</p>', 'blog-article-four' ,'2017-02-04' , 1 ,1 ,'2017-01-14')
+GO
+
+INSERT INTO [dbo].[Blog]
+           ([Title]
+           ,[Subtitle]
+           ,[Body]
+           ,[UniqueURL]
+           ,[PublishDate]
+           ,[AuthorID]
+           ,[Active]
+           ,[CreateDate])
+     VALUES
+           ('Blog Article #5' ,'Subtitle Test' ,'<p>body!</p>', 'blog-article-five' ,'2017-02-28' , 1 ,1 ,'2017-01-14')
+GO
+
+INSERT INTO [dbo].[Blog]
+           ([Title]
+           ,[Subtitle]
+           ,[Body]
+           ,[UniqueURL]
+           ,[PublishDate]
+           ,[AuthorID]
+           ,[Active]
+           ,[CreateDate])
+     VALUES
+           ('Blog Article #6' ,'Subtitle Test' ,'<p>body!</p>', 'blog-article-six' ,'2017-03-17' , 1 ,1 ,'2017-01-14')
+GO
