387a780d47263e7cd6031e2acf142ff5a6ec666c
bmbruno <bmbruno@gmail.com>
2017-01-22 07:23:59 -0500

    Create basic ISearchService interface.

---
+++ Application/SparkServer.Core/Services/ISearchService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkServer.Core.Repositories
+{
+    public interface ISearchService
+    {
+        void Initialize();
+
+        void Query(string term);
+    }
+}
