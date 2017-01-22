cd8e64140d193360ce1857a83a08d2f714e7b4dd
bmbruno <bmbruno@gmail.com>
2017-01-22 08:00:32 -0500

    Added HomeIndexViewModel and view output.

---
+++ Application/SparkServer/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SparkServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkServer.ViewModels
+{
+    public class HomeIndexViewModel : BaseViewModel
+    {
+        public List<ArticleViewModel> ArticleList { get; set; }
+
+        public List<BlogArticleViewModel> BlogList { get; set; }
+
+        public HomeIndexViewModel()
+        {
+            ArticleList = new List<ArticleViewModel>();
+            BlogList = new List<BlogArticleViewModel>();
+        }
+    }
+}
