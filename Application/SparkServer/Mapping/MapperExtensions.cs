93b0001245464ed414f113c7fe800968d8cbf742
bmbruno <bmbruno@gmail.com>
2017-04-08 08:51:36 -0400

    Added new logo. Style adjustments for new logo. Fixed wrong Category page link on main menu.

--- Application/SparkServer/Mapping/MapperExtensions.cs
+++ Application/SparkServer/Mapping/MapperExtensions.cs
@@ -66,7 +66,7 @@ namespace SparkServer.Mapping
                         {
                             ArticleID = article.ID,
                             ArticleTitle = article.Title,
-                            URL = $"/Article/{article.UniqueURL}"
+                            URL = $"/article/{article.UniqueURL}"
                         });
                     }
 
