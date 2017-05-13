f62ffee89e04fc630fb485c60b37976f3b5e2a82
bmbruno <bmbruno@gmail.com>
2017-05-13 07:19:50 -0400

    Added "Today" button with functionality to get the current date's value.

--- Application/SparkServer/Scripts/admin.js
+++ Application/SparkServer/Scripts/admin.js
@@ -1,5 +1,12 @@
 $(function () {
 
-    
+    $("#DateTimeToday").on("click", function setDateToToday() {
+
+        var currentDateTime = new Date();
+        var output = currentDateTime.toLocaleDateString();
+
+        $("#PublishDate").val(output);
+
+    });
 
 });
