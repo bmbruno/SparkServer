cbaf274d000ab91d1369b1fbc83b9d86c5268c9e
bmbruno <bmbruno@gmail.com>
2017-10-12 10:17:55 -0400

    Added FormatHelper class.

---
+++ Application/SparkServer/Application/Helpers/FormatHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkServer.Application.Helpers
+{
+    public static class FormatHelper
+    {
+        /// <summary>
+        /// Turns a proper phrase/sentence into URL-friendly format. "Hello, Great Big World" -> "hello-great-big-world"
+        /// </summary>
+        /// <param name="input">Input string to transform.</param>
+        /// <returns>URL-ready string.</returns>
+        public static string ConvertToUrlFormat(string input)
+        {
+            input = input.ToLower();
+            input = input.Replace(" ", "-");
+
+            return "";
+        }
+    }
+}
