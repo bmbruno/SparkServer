eec429e4038cf3c161474c327413d075907b60c9
bmbruno <bmbruno@gmail.com>
2018-01-30 09:51:08 -0500

    Fixed related article/links SQL DDL.

--- SQLScripts/DDL-DML/1.4/010. CREATE TABLE ArticleLinks.sql
+++ SQLScripts/DDL-DML/1.4/010. CREATE TABLE ArticleLinks.sql
@@ -1,12 +1,12 @@
 USE [SparkServer]
 GO
 
-CREATE TABLE [ArticleLinks] (
+CREATE TABLE [RelatedArticleLinks] (
 	
 	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 
 	[ArticleID] INT NOT NULL,
-	CONSTRAINT [FK_ArticleLinks_ArticleSource] FOREIGN KEY ([ArticleID]) REFERENCES [Article]([ID]),
+	CONSTRAINT [FK_RelatedArticleLinks_ArticleSource] FOREIGN KEY ([ArticleID]) REFERENCES [Article]([ID]),
 	
 	[Name] VARCHAR(250) NOT NULL,
 	
