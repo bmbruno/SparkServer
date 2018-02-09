5f954809d8eb3f1748b655ee809642864292d8c6
bmbruno <bmbruno@gmail.com>
2018-02-09 11:18:01 -0500

    Prettied up a file.

--- Application/SparkServer/ViewModels/Admin/RelatedLinkItemViewModel.cs
+++ Application/SparkServer/ViewModels/Admin/RelatedLinkItemViewModel.cs
@@ -1,21 +1,22 @@
 ﻿using SparkServer.Application.Enum;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace SparkServer.ViewModels
 {
     public class RelatedLinkItemViewModel : BaseViewModel
-    {
-        private int? _SortOrder;
-
+    {        
         public int ID { get; set; }
 
+        [Display(Name = ""]
         public string Title { get; set; }
 
         public string HREF { get; set; }
-
+        
+        private int? _SortOrder;
         public int? SortOrder
         {
             get { if (this._SortOrder.HasValue) { return this._SortOrder; } else { return 0; } }
