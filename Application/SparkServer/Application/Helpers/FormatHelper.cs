df556df53cb6d8399f274179b99035bec2e8c05f
bmbruno <bmbruno@gmail.com>
2018-11-12 11:32:25 -0500

    Added HtmlAgilityPack for HTML parsing. Added SparkFormatProcessor to handle HTML formatting rules. Added formatting rule to process external links.

--- Application/SparkServer/Application/Helpers/FormatHelper.cs
+++ Application/SparkServer/Application/Helpers/FormatHelper.cs
@@ -1,4 +1,6 @@
 ﻿using Markdig;
+using HtmlAgilityPack;
+using System.IO;
 
 namespace SparkServer.Application.Helpers
 {
@@ -17,11 +19,23 @@ namespace SparkServer.Application.Helpers
             return "";
         }
 
+        /// <summary>
+        /// Converts a string of Markdown to valid HTML for hosting 
+        /// </summary>
+        /// <param name="markdownInput"></param>
+        /// <returns></returns>
         public static string MarkdownToHTML(string markdownInput)
-        {
-            string output = Markdown.ToHtml(markdownInput);
+        {           
+            string htmlValue = Markdown.ToHtml(markdownInput);
 
-            return output;
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlValue);
+
+            htmlDoc = SparkFormatProcessor.ExternalLinksGetBlankTarget(htmlDoc);
+
+            // TODO: images: a) if image tag is not proceeded by a <div> with class of "figure", add "figure" div; b) if image is proceeded by a <div> with class "figure", then ignore and move on
+            
+            return htmlDoc.DocumentNode.OuterHtml;
         }
     }
 }
---
+++ Application/SparkServer/Application/Helpers/SparkFormatProcessor.cs
@@ -0,0 +1,41 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkServer.Application.Helpers
+{
+    public static class SparkFormatProcessor
+    {
+        /// <summary>
+        /// Adds target="_blank" attribute to all anchor tags that reference resources external to the domain name configured in AppSettings (use Config.DomainName in code to get at the value).
+        /// </summary>
+        /// <param name="htmlDoc">HtmlAgilityPack HtmlDocument object.</param>
+        /// <returns>HtmlDocument object with anchor tags processed.</returns>
+        public static HtmlDocument ExternalLinksGetBlankTarget(HtmlDocument htmlDoc)
+        {
+            var anchors = htmlDoc.DocumentNode.SelectNodes("//a");
+
+            foreach (var anchor in anchors)
+            {
+                string originalHTML = anchor.OuterHtml;
+
+                if (anchor.Attributes["href"] != null)
+                {
+                    string anchorValue = anchor.Attributes["href"].Value;
+
+                    if (!anchorValue.Contains(Config.DomainName) && !anchorValue.StartsWith("/"))
+                    {
+                        if (anchor.Attributes["target"] == null)
+                        {
+                            anchor.SetAttributeValue("target", "_blank");
+                        }
+                    }
+                }
+            }
+
+            return htmlDoc;
+        }
+    }
+}
