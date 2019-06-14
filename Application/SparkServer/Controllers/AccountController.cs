f79a6e253ae915202193327ca1716c83fb04cdd9
bmbruno <bmbruno@gmail.com>
2019-06-14 08:10:30 -0400

    SSO integration.

--- Application/SparkServer/Controllers/AccountController.cs
+++ Application/SparkServer/Controllers/AccountController.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using QuickSSO.Client;
+using SparkServer.Application;
+using System;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace SparkServer.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
-        public ActionResult Index()
+        public ActionResult Logout()
         {
-            return View();
+            FormsAuthentication.SignOut();
+
+            return Redirect(url: Config.SSOLogoutURL + Config.SiteID);
+        }
+
+        public ActionResult Login()
+        {
+            // Request login/authentication from sso.brandonbrun.com
+            return Redirect(url: Config.SSOLoginURL + Config.SiteID);
+        }
+
+        public ActionResult Authenticate(string token)
+        {
+            if (String.IsNullOrEmpty(token))
+            {
+                TempData["Info"] = "Token is empty.";
+                return RedirectToAction(actionName: "Index", controllerName: "Home");
+            }
+
+            if (!TokenService.TokenIsValid(token, Config.SigningKey))
+            {
+                TempData["Info"] = "Token not valid. Possible signature error.";
+                return RedirectToAction(actionName: "Index", controllerName: "Home");
+            }
+
+            TokenPayload payload = TokenService.GetPayload(token);
+
+            FormsAuthentication.SetAuthCookie(payload.uid.ToString(), true);
+
+            return RedirectToAction(actionName: "Index", controllerName: "Admin");
         }
     }
 }
